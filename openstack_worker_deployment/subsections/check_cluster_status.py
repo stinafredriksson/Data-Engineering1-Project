ba2c65875030fe47c019ba92c6274464ccff2f3c
Erik <147971481+erikeken@users.noreply.github.com>
2024-03-16 19:47:44 +0100

    Update check_cluster_status.py
    Added comments to explain the function

--- openstack_worker_deployment/subsections/check_cluster_status.py
+++ openstack_worker_deployment/subsections/check_cluster_status.py
@@ -1,7 +1,8 @@
+# This is just a script for checking the cluster usage if autoworkerdeployment workes
 import requests
 
 # Replace with your Spark master's web UI address and port
-spark_master_web_ui = 'http://130.238.28.228:8080'
+spark_master_web_ui = 'http://130.238.28.228:8080' # can change to local ip instead when run on master
 
 response = requests.get(f"{spark_master_web_ui}/json/")
 cluster_info = response.json()
